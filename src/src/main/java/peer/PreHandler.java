4e4a2d0d348cb44edf71f25f44796c7848a6711f
up201806206 <lmiguelapinto@gmail.com>
2021-05-14 12:03:26 +0100

    added constant instead of literal buffer size

--- src/src/main/java/peer/PreHandler.java
+++ src/src/main/java/peer/PreHandler.java
@@ -1,59 +1,48 @@
 package peer;
 
-import java.io.DataInputStream;
-import java.io.EOFException;
-import java.io.IOException;
-import java.util.Arrays;
-
 import javax.net.ssl.SSLSocket;
+import java.io.DataInputStream;
+import java.io.IOException;
 
-public class Prehandler implements Runnable {
+public class PreHandler implements Runnable {
+    private static final int bufferSize = 512;
     private SSLSocket socket;
     private int peerId;
     private byte[] message;
     private int currentMessageSize;
     private int actualMessageSize;
 
-    public Prehandler(SSLSocket socket, int peerId)
-    {
+    public PreHandler(SSLSocket socket, int peerId) {
         this.socket = socket;
         this.peerId = peerId;
-        this.currentMessageSize = 512;
-        this.message = new byte[currentMessageSize];
+        this.currentMessageSize = bufferSize;
+        this.message = new byte[this.currentMessageSize];
         this.actualMessageSize = 0;
     }
 
     @Override
     public void run() {
         DataInputStream stream = null;
-        try
-        {
+        try {
             stream = new DataInputStream(this.socket.getInputStream());
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Failed to read from socket in Prehandler!");
             e.printStackTrace();
         }
 
         int currentIndex = 0;
-        
+
         int numReadBytes;
-        while (true)
-        {
-            try
-            {
-                numReadBytes = stream.read(this.message, currentIndex, 512);
-            }
-            catch (IOException e)
-            {
+        while (true) {
+            try {
+                numReadBytes = stream.read(this.message, currentIndex, bufferSize);
+            } catch (IOException e) {
                 System.out.println("Failed to read from input stream in Prehandler!");
                 e.printStackTrace();
 
-                currentIndex += 512;
-                this.currentMessageSize += 512;
+                this.currentMessageSize += bufferSize;
                 byte[] auxBuffer = new byte[this.currentMessageSize];
-                System.arraycopy(this.message, 0, auxBuffer, 0, this.currentMessageSize - 512);
+                System.arraycopy(this.message, 0, auxBuffer, 0, this.currentMessageSize - bufferSize);
                 this.message = auxBuffer;
 
                 break;
@@ -61,23 +50,21 @@ public class Prehandler implements Runnable {
             if (numReadBytes == -1) break;
             this.actualMessageSize += numReadBytes;
 
-            currentIndex += 512;
-            this.currentMessageSize += 512;
+            currentIndex += bufferSize;
+            this.currentMessageSize += bufferSize;
             byte[] auxBuffer = new byte[this.currentMessageSize];
-            System.arraycopy(this.message, 0, auxBuffer, 0, this.currentMessageSize - 512);
+            System.arraycopy(this.message, 0, auxBuffer, 0, this.currentMessageSize - bufferSize);
             this.message = auxBuffer;
         }
 
-        processMessage();
+        this.processMessage();
     }
 
 
-    private void processMessage()
-    {
-        if (this.actualMessageSize < this.message.length)
-        {
+    private void processMessage() {
+        if (this.actualMessageSize < this.message.length) {
             byte[] auxBuffer = new byte[this.actualMessageSize];
-            System.arraycopy(this.message, 0, auxBuffer, 0,this.actualMessageSize);
+            System.arraycopy(this.message, 0, auxBuffer, 0, this.actualMessageSize);
             this.message = auxBuffer;
         }
 
@@ -86,5 +73,5 @@ public class Prehandler implements Runnable {
         Handler handler = new Handler(this.message, this.peerId);
         handler.processMessage();
     }
-    
+
 }
