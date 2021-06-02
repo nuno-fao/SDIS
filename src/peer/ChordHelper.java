b05f5114a769bfe46fc91143fdfcccdf410c8f93
up201806206 <lmiguelapinto@gmail.com>
2021-06-02 10:48:50 +0100

    fixed chord bug

--- src/peer/ChordHelper.java
+++ src/peer/ChordHelper.java
@@ -7,8 +7,13 @@ public class ChordHelper implements Runnable {
         this.chord = chord;
     }
 
+    static int s = 0;
+
     @Override
     public void run() {
+        System.out.println("1: "+this.chord.getSuccessor().id);
+        System.out.println(this.chord.getPredecessor());
+        System.out.println(s++);
         this.chord.Stabilize();
         this.chord.FixFingers();
         this.chord.CheckPredecessor();
